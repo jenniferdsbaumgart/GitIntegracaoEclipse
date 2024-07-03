373479a110342ced5563eacd06bb474f0d4d1b91
jenniferdsbaumgart <jenniferdsbaumgart@gmail.com>
2024-07-03 15:16:13 -0300

    Segundo commit e push vindo do Eclipse

--- GitIntegracaoEclipse/src/gitIntegracao/TesteGitEclipse.java
+++ GitIntegracaoEclipse/src/gitIntegracao/TesteGitEclipse.java
@@ -5,6 +5,7 @@ public class TesteGitEclipse {
 	public static void main(String[] args) {
 		System.out.println("Teste da integração da IDE Eclipse com o Git");
 		System.out.println("Alterando do GitHub para o Eclipse");
+		System.out.println("Segundo commit");
 	}
 
 }
