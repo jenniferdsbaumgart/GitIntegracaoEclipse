e0836de6f8aa811c6f5fca1954bda13c84f57889
Jennifer Da Silva Baumgart <125690937+jenniferdsbaumgart@users.noreply.github.com>
2024-07-03 15:09:30 -0300

    Alterando no GitHub para o Eclipse.java

--- GitIntegracaoEclipse/src/gitIntegracao/TesteGitEclipse.java
+++ GitIntegracaoEclipse/src/gitIntegracao/TesteGitEclipse.java
@@ -4,6 +4,7 @@ public class TesteGitEclipse {
 
 	public static void main(String[] args) {
 		System.out.println("Teste da integração da IDE Eclipse com o Git");
+		System.out.println("Alterando do GitHub para o Eclipse");
 	}
 
 }
